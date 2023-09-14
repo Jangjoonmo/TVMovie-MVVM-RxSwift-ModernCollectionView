5508dacfcebc246e6f0b496a66459544398cb209
jangjoonmo <jjmo97@ajou.ac.kr>
2023-09-15 01:25:25 +0900

    Initial Commit

---
+++ TVMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TVMovie
+//
+//  Created by 장준모 on 2023/09/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
