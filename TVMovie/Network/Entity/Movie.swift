ec49f59261eb2aca3d4b8de26f007f03864bda4e
jangjoonmo <jjmo97@ajou.ac.kr>
2023-09-17 19:32:22 +0900

    feat: Movie Entity 구현

--- TVMovie/Network/Entity/Movie.swift
+++ TVMovie/Network/Entity/Movie.swift
@@ -1,5 +1,5 @@
 //
-//  TV.swift
+//  Movie.swift
 //  TVMovie
 //
 //  Created by 장준모 on 2023/09/17.
@@ -7,37 +7,36 @@
 
 import Foundation
 
-struct TVListModel: Decodable {
+struct MovieListModel: Decodable {
     let page: Int
-    let results: [TV]
-    
+    let results: [Movie]
 }
 
-struct TV: Decodable{
-    let name: String
+struct Movie: Decodable {
+    let title: String
     let overview: String
     let posterURL: String
     let vote: String
-    let firstAirDate: String
+    let releaseDate: String
     
     private enum CodingKeys: String, CodingKey {
-        case name
+        case title
         case overview
         case posterPath = "poster_path"
         case voteAverage = "vote_average"
         case voteCount = "vote_count"
-        case firstAirDate = "first_air_date"
+        case releaseDate = "release_date"
     }
     
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        name = try container.decode(String.self, forKey: .name)
+        title = try container.decode(String.self, forKey: .title)
         overview = try container.decode(String.self, forKey: .overview)
         let path = try container.decode(String.self, forKey: .posterPath)
         posterURL = "https://image.tmdb.org/t/p/w500\(path)"
         let voteAverage = try container.decode(String.self, forKey: .voteAverage)
         let voteCount = try container.decode(String.self, forKey: .voteCount)
         vote = "\(voteAverage) (\(voteCount)"
-        firstAirDate = try container.decode(String.self, forKey: .firstAirDate)
+        releaseDate = try container.decode(String.self, forKey: .releaseDate)
     }
 }
---
+++ TVMovie/Network/Entity/MovieResult.swift
@@ -0,0 +1,14 @@
+//
+//  MovieResult.swift
+//  TVMovie
+//
+//  Created by 장준모 on 2023/09/17.
+//
+
+import Foundation
+
+struct MovieResult {
+    let upcoming: MovieListModel
+    let popular: MovieListModel
+    let nowPlaying: MovieListModel
+}
