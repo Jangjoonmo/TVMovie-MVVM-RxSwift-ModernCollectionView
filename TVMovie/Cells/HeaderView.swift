2385358c39e6e664d9ec7627d8f0f410fe69f157
jangjoonmo <jjmo97@ajou.ac.kr>
2023-09-19 21:46:28 +0900

    feat: Collection View 헤더 구현

---
+++ TVMovie/Cells/HeaderView.swift
@@ -0,0 +1,34 @@
+//
+//  HeaderView.swift
+//  TVMovie
+//
+//  Created by 장준모 on 2023/09/19.
+//
+
+import Foundation
+import UIKit
+
+final class HeaderView: UICollectionReusableView {
+    static let id = "HeaderView"
+    private let titleLabel: UILabel = {
+       let label = UILabel()
+        label.font = .systemFont(ofSize: 20, weight: .bold)
+        return label
+    }()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(titleLabel)
+        
+        titleLabel.snp.makeConstraints{
+            $0.horizontalEdges.top.equalToSuperview()
+        }
+    }
+    
+    public func configure(title: String) {
+        titleLabel.text = title
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
