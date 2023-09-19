69a02f7d086f168b7fe4c696af463345e001a3b5
jangjoonmo <jjmo97@ajou.ac.kr>
2023-09-19 21:42:26 +0900

    feat: 리스트 타입 셀 구현

---
+++ TVMovie/Cells/ListCollectionViewCell.swift
@@ -0,0 +1,62 @@
+//
+//  ListCollectionViewCell.swift
+//  TVMovie
+//
+//  Created by 장준모 on 2023/09/19.
+//
+
+import Foundation
+import UIKit
+
+final class ListCollectionViewCell: UICollectionViewCell {
+    static let id = "ListCollectionViewCell"
+    
+    let image: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = 8
+        return imageView
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 20, weight: .bold)
+        return label
+    }()
+    
+    let releaseDateLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14, weight: .regular)
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(image)
+        addSubview(titleLabel)
+        addSubview(releaseDateLabel)
+        
+        image.snp.makeConstraints{
+            $0.leading.top.bottom.equalToSuperview()
+            $0.width.equalToSuperview().multipliedBy(0.3)
+        }
+        titleLabel.snp.makeConstraints{
+            $0.leading.equalTo(image.snp.trailing).offset(8)
+            $0.top.equalToSuperview()
+        }
+        releaseDateLabel.snp.makeConstraints{
+            $0.leading.equalTo(titleLabel.snp.leading)
+            $0.top.equalTo(titleLabel.snp.bottom).offset(4)
+        }
+    }
+    
+    public configure(title: String, releaseDate: String, url: String) {
+        titleLabel.text = title
+        releaseDateLabel.text = releaseDate
+        image.kf.setImage(with: URL(string: url))
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
